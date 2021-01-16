cb778f8a324bf1095e0edb145120d9ef3728882e
Nick <dreadnaughtlin@gmail.com>
2021-01-16 19:26:37 +0800

    use latin name

--- Typing_Game/script.js
+++ Typing_Game/script.js
@@ -1,12 +1,12 @@
+// Generate random word
 function randomWords() {
     fetch('https://data.taipei/api/v1/dataset/f18de02f-b6c9-47c0-8cda-50efad621c14?scope=resourceAquire')
         .then(res => res.json())
         .then(data => {
             const words = data.result.results;
-            let word = words[Math.floor(Math.random() * words.length)].F_Name_En;
+            let word = words[Math.floor(Math.random() * words.length)].F_Name_Latin;
             $('h1').text(word);
         })
-
 }
 
 randomWords();
