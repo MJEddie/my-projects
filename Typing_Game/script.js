c6523c05616e77b39c70cfe9b3d5e3ba723985a4
Nick <dreadnaughtlin@gmail.com>
2021-01-16 18:23:37 +0800

    fetch API

---
+++ Typing_Game/script.js
@@ -0,0 +1,11 @@
+function randomWords() {
+    fetch('https://data.taipei/api/v1/dataset/f18de02f-b6c9-47c0-8cda-50efad621c14?scope=resourceAquire')
+        .then(res => res.json())
+        .then(data => {
+            const words = data.result.results;
+
+        })
+
+}
+
+randomWords();
