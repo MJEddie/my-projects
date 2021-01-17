2718231e26b51fad0cfe002f6435cb371b7eb2c9
Nick <dreadnaughtlin@gmail.com>
2021-01-17 23:35:26 +0800

    implement display balance

--- Expense_Tracker/script.js
+++ Expense_Tracker/script.js
@@ -18,15 +18,34 @@ function addTransactionDOM(transaction) {
 
     item.html(`${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
      <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>`);
-
-    $('#list').append(item);
 }
 
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+    ).toFixed(2);
+
+    $('#balance').text(`$${total}`);
+    $('#money-plus').text(`$${income}`);
+    $('#money-minus').text(`$${expense}`);
+}
 // Init app
 function init() {
     $('#list').html('');
 
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
