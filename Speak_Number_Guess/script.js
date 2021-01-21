3f918c7de1c3e0a280224da74afcd5aa6d6a4e69
Nick <dreadnaughtlin@gmail.com>
2021-01-21 23:31:22 +0800

    check guessed number

--- Speak_Number_Guess/script.js
+++ Speak_Number_Guess/script.js
@@ -16,7 +16,32 @@ function randomNumber() {
 function onSpeak(e) {
     const msg = e.results[0][0].transcript;
 
+    checkNumber(msg);
     console.log(msg)
+}
+
+function checkNumber(msg) {
+    const number = parseInt(msg);
+    $('#msg').html(`
+            <div>You said: </div>
+            <span class="box">${msg}</span>
+        `)
+
+    if (isNaN(number)) {
+        $('#msg').append(`<div>That is not a valid number</div>`);
+    } else if (number > 100 || number < 1) {
+        $('#msg').append(`<div>Number must be between 1 and 100</div>`)
+    } else if (number == randomNumber) {
+        $('body').html(`
+            <h2>Congrats! You have guessed the number! <br><br>
+            It was ${number}</h2>
+            <button class="play-again" id="play-again">Play Again</button>
+        `)
+    } else if (number > randomNumber) {
+        $('msg').append('<div>GO LOWER</div>');
+    } else {
+        $('msg').append('<div>GO HIGHER</div>');
+    }
 }
 
 // Event linstener
