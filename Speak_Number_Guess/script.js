f7b7bb66b5eaf3aa24d938748a79323604fdc436
Nick <dreadnaughtlin@gmail.com>
2021-01-21 23:12:01 +0800

    capture mic input

--- Speak_Number_Guess/script.js
+++ Speak_Number_Guess/script.js
@@ -2,7 +2,22 @@ const finalNumber = randomNumber();
 
 console.log(finalNumber)
 
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new SpeechRecognition();
+
+recognition.start();
+
 // Generate random number
 function randomNumber() {
     return Math.floor(Math.random() * 100) + 1;
 }
+
+function onSpeak(e) {
+    const msg = e.results[0][0].transcript;
+
+    console.log(msg)
+}
+
+// Event linstener
+recognition.addEventListener('result', onSpeak);
