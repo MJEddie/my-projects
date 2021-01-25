a57f32ee16a3dd20fcba497ec8a9c7f4d7c186cd
MJEddie <dreadnaughtlin@gmail.com>
2021-01-25 17:25:22 +0800

    scramble list items

--- Sortable_List/script.js
+++ Sortable_List/script.js
@@ -10,3 +10,24 @@ const famousFood = [
     '刨冰',
     '鳳梨酥'
 ];
+
+const listItems = [];
+creatList();
+
+function creatList() {
+    [...famousFood]
+    .map(data => ({ value: data, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(data => data.value)
+        .forEach((food, index) => {
+            const listItem = $('<li></li>').attr('data-index', index);
+            listItem.html(`
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="food-name">${food}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `);
+            $('#draggable-list').append(listItem);
+        })
+}
