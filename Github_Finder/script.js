afa7e2f7809d9a121d682d01501aa53e01ae1904
MJEddie <dreadnaughtlin@gmail.com>
2021-01-29 01:51:46 +0800

    add user name display

--- Github_Finder/script.js
+++ Github_Finder/script.js
@@ -9,19 +9,35 @@ function getUserData() {
         dataType: 'json',
         success: function(res) {
             let obj = {};
-            obj.avatar_url = res.avatar_url !== null ? res.avatar_url : ''
-            obj.html_url = res.html_url !== null ? res.html_url : ''
-            obj.public_repos = res.public_repos !== null ? res.public_repos : ''
-            obj.public_gists = res.public_gists !== null ? res.public_gists : ''
-            obj.followers = res.followers !== null ? res.followers : ''
-            obj.following = res.following !== null ? res.following : ''
-            obj.company = res.company !== null ? res.company : ''
-            obj.blog = res.blog !== null ? res.blog : ''
-            obj.location = res.location !== null ? res.location : ''
-            obj.created_at = res.created_at.substr(0, 10) !== null ? res.created_at.substr(0, 10) : ''
-            obj.updated_at = res.updated_at.substr(0, 10) !== null ? res.updated_at.substr(0, 10) : ''
+            obj.avatar_url = res.avatar_url !== null ? res.avatar_url : '';
+            obj.name = res.name !== null ? res.name : '';
+            obj.html_url = res.html_url !== null ? res.html_url : '';
+            obj.public_repos = res.public_repos !== null ? res.public_repos : '';
+            obj.public_gists = res.public_gists !== null ? res.public_gists : '';
+            obj.followers = res.followers !== null ? res.followers : '';
+            obj.following = res.following !== null ? res.following : '';
+            obj.company = res.company !== null ? res.company : '';
+            obj.blog = res.blog !== null ? res.blog : '';
+            obj.location = res.location !== null ? res.location : '';
+            obj.created_at = res.created_at.substr(0, 10) !== null ? res.created_at.substr(0, 10) : '';
+            obj.updated_at = res.updated_at.substr(0, 10) !== null ? res.updated_at.substr(0, 10) : '';
             showProfile(obj);
+            console.log(res)
         }
+    });
+}
+
+function getRepoData() {
+    $.ajax({
+        url: `https://api.github.com/users/${username}/repos?per_page=5&sort=created: asc`,
+        method: 'GET',
+        dataType: 'json',
+        success: function(res) {
+            data = res;
+            console.log(data)
+                // showRepos(data);
+        }
+
     });
 }
 
@@ -56,6 +72,31 @@ function showProfile(user) {
             <div id="repos"></div>
         </div>
     `);
+}
+
+//Show repo
+function showRepos(repos) {
+    let output = '';
+    repos.forEach(function(repo) {
+        $('#repos').html(`
+            <div class="card card-body mb-2  border-0">
+                <div class="row shadow-sm p-3 bg-white rounded">
+                    <div class="col-md-6">
+                        <a href="${repo.html_url}" target="_blank">
+                            <h3>${repo.name}</h3>
+                        </a>
+                        <p>${repo.description}</p>
+                    </div>
+                    <div class="col-md-6">
+                        <span class="badge badge-warning m-1">Stars ${repo.stargazers_count}</span>
+                        <span class="badge badge-success m-1">Watchers ${repo.watchers_count}</span>
+                        <span class="badge badge-info m-1">Forks ${repo.forks_count}</span>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+    $('')
 }
 
 searchUser.keyup(function() {
