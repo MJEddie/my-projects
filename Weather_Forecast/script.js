a64595cf75eb37b839733c9799fb803413bd9596
MJEddie <dreadnaughtlin@gmail.com>
2021-02-04 00:50:40 +0800

    implement date display

--- Weather_Forecast/script.js
+++ Weather_Forecast/script.js
@@ -2,15 +2,16 @@ let city;
 let cityIndex = 9;
 let data;
 
+const timeNow = new Date();
+
 function getWeather() {
     $.ajax({
-        url: 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-373C6328-6BF2-41B3-BB3B-147802B82875&format=JSON&locationName=&elementName=&sort=time',
+        url: 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-373C6328-6BF2-41B3-BB3B-147802B82875&format=JSON&locationName=&elementName=&sort=time&timeFrom=2021-02-04T06%3A00%3A00&timeTo=2021-02-04T18%3A00%3A00" -H  "accept: application/json',
         method: 'GET',
         dataType: 'json',
         success: function(res) {
             data = res.records.locations[0];
             city = data.location;
-
             console.log(data)
             selectCity(city);
             showWeather(data, cityIndex)
@@ -35,10 +36,10 @@ function showWeather(data, cityIndex) {
     const description = weather[6].time[0].elementValue[0].value;
     const minT = `${weather[8].time[0].elementValue[0].value} °C`;
     const maxT = `${weather[12].time[0].elementValue[0].value} °C`;
-    console.log(maxT)
+    const date = timeNow.toGMTString();
     $('.description').html(`
     <h1>${cityName}</h1>
-    <h2>Monday, 1 February 2021</h2>
+    <h2>${date.substr(0,16)}</h2>
     <img src="images/svg/sun.svg" alt="weather-img">
     <div class="today-description">${description} <br>${minT} / ${maxT}</div>
     `)
