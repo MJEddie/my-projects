bca0b2da769c2e45cb9d09b5ff2b169c75a3d90e
MJEddie <dreadnaughtlin@gmail.com>
2021-02-04 00:02:07 +0800

    implement today's weather

--- Weather_Forecast/script.js
+++ Weather_Forecast/script.js
@@ -1,4 +1,6 @@
 let city;
+let cityIndex = 9;
+let data;
 
 function getWeather() {
     $.ajax({
@@ -6,9 +8,12 @@ function getWeather() {
         method: 'GET',
         dataType: 'json',
         success: function(res) {
-            city = res.records.locations[0].location;
-            console.log(res, city)
+            data = res.records.locations[0];
+            city = data.location;
+
+            console.log(data)
             selectCity(city);
+            showWeather(data, cityIndex)
         }
     });
 }
@@ -24,4 +29,24 @@ function selectCity(data) {
     }
 }
 
+function showWeather(data, cityIndex) {
+    cityName = data.location[cityIndex].locationName;
+    const weather = data.location[cityIndex].weatherElement;
+    const description = weather[6].time[0].elementValue[0].value;
+    const minT = `${weather[8].time[0].elementValue[0].value} °C`;
+    const maxT = `${weather[12].time[0].elementValue[0].value} °C`;
+    console.log(maxT)
+    $('.description').html(`
+    <h1>${cityName}</h1>
+    <h2>Monday, 1 February 2021</h2>
+    <img src="images/svg/sun.svg" alt="weather-img">
+    <div class="today-description">${description} <br>${minT} / ${maxT}</div>
+    `)
+}
+
 getWeather();
+
+$('#select').change(function() {
+    cityIndex = $('#select :selected').val();
+    showWeather(data, cityIndex);
+});
