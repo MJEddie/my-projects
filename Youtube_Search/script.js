f944aea6b71193a99a26f917ac6d7246b31bda7f
MJEddie <dreadnaughtlin@gmail.com>
2021-02-05 22:45:17 +0800

    implement search results display

--- Youtube_Search/script.js
+++ Youtube_Search/script.js
@@ -15,6 +15,35 @@ function getVideos() {
         dataType: 'json',
         success: function(res) {
             console.log(res);
+
+            $.each(res.items, function(i, item) {
+                showViedos(item);
+            })
         }
     });
+}
+
+function showViedos(item) {
+
+    const videoID = item.id.videoId;
+    const title = item.snippet.title;
+    const description = item.snippet.description;
+    const thumbnail = item.snippet.thumbnails.high.url;
+    const channelTitle = item.snippet.channelTitle;
+    const videoDate = item.snippet.publishedAt;
+
+    const result = $('<li></li>');
+    result.html(`
+        <div class="thumbnail">
+            <img src="${thumbnail}" alt="">
+        </div>
+        <div class="info">
+            <h3><a href="http://www.youtube.com/embed/${videoID}">${title}</a></h3>
+            <small>By <span class="cTitle">${channelTitle}</span> on ${videoDate.substr(0,10)}</small>
+            <p>${description}</p>
+        </div>
+        `);
+
+    $('#results').append(result);
+
 }
